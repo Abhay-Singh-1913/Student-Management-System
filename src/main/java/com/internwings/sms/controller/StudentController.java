f461371ef2093377535f54d651967bd9909f4624
Abhay Singh <93770368+Abhay-Singh-1913@users.noreply.github.com>
2024-01-19 06:04:10 +0000

    updated

--- src/main/java/com/internwings/sms/controller/StudentController.java
+++ src/main/java/com/internwings/sms/controller/StudentController.java
@@ -1,5 +1,17 @@
 package com.internwings.sms.controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class StudentController {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pgstudent");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		
+    }
 
 }
