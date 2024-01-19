c7eca7d70aac23c84c1ec4d7b17eb4d4447f36e7
Abhay Singh <93770368+Abhay-Singh-1913@users.noreply.github.com>
2024-01-19 11:41:17 +0530

    Update StudentController.java

--- src/main/java/com/internwings/sms/controller/StudentController.java
+++ src/main/java/com/internwings/sms/controller/StudentController.java
@@ -1,17 +1,56 @@
 package com.internwings.sms.controller;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import com.internwings.sms.model.Student;
 
 public class StudentController {
-
-    public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pgstudent");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		EntityTransaction entityTransaction = entityManager.getTransaction();
 		
-    }
+		static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pgstudent");
+		static EntityManager entityManager = entityManagerFactory.createEntityManager();
+		static EntityTransaction entityTransaction = entityManager.getTransaction();
+		
 
+		public boolean addStudent(Student student) {
+			if (student != null) {
+				entityTransaction.begin();
+				entityManager.persist(student);
+				entityTransaction.commit();
+				return true;
+			}
+			return false;
+		}
+		
+		public void update(String name, double grade) {
+			if (name != null) {
+				
+			}
+		}
+		
+		public boolean deleteStudent(Student student) {
+			if (student != null) {
+				entityTransaction.begin();
+				entityManager.remove(student);
+				entityTransaction.commit();
+				return true;
+			}
+			return false;
+		}
+		
+		public Student searchStudent(int rollno) {
+			return entityManager.find(Student.class, rollno);
+		}
+		
+		public List<Student> displayAllStudents() {
+			String jpql = "SELECT std FORM Student std";
+			TypedQuery<Student> query = entityManager.createQuery(jpql, Student.class);
+			return query.getResultList();
+		}
+		
 }
