823ae80a87fd075890bc0c1cdffe45d8f18e613c
Abhay Singh <93770368+Abhay-Singh-1913@users.noreply.github.com>
2024-01-22 18:22:54 +0530

    Update Student_Management_System.java

--- src/main/java/com/internwings/sms/view/Student_Management_System.java
+++ src/main/java/com/internwings/sms/view/Student_Management_System.java
@@ -12,7 +12,37 @@ public class Student_Management_System {
 		System.out.println("//////////////////////////////////////////////////////////");
 	}
 	public static void main(String[] args) {
-		
+		final String red = "\u001b[31;1m";
+		final String white = "\u001b[37;1m";
+		final String green = "\u001b[32;1m";
+		final String reset = "\u001b[0m";
+		final String yellow = "\u001b[33m";
+
+		boolean Flag = true;
+		while (Flag) {
+			System.out.println(yellow);
+			System.out.println(" ___________________________________ ");
+			System.out.println("|                                   |");
+			System.out.println("|     ==== Welcome To MENU ====     |");
+			System.out.println("|___________________________________|");
+			System.out.println("|                                   |");
+			System.out.println("|  1. Add new Student               |");
+			System.out.println("|                                   |");
+			System.out.println("|  2. Update Student Details        |");
+			System.out.println("|                                   |");
+			System.out.println("|  3. Search Student by Rollno.     |");
+			System.out.println("|                                   |");
+			System.out.println("|  4. Display all the Student       |");
+			System.out.println("|                                   |");
+			System.out.println("|  0. Exit                          |");
+			System.out.println("|___________________________________|" + reset);
+			System.out.println(white);
+			System.out.println(" =================================== ");
+			System.out.print("| Enter Your Choice : ");
+			byte choice = myInput.nextByte();
+			myInput.nextLine();
+			System.out.println(" =================================== ");
+			System.out.println();
 	}
 
 }
