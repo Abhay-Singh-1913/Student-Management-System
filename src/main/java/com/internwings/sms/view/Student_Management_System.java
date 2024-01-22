ec393e7c42fdc108353544fb70bb40632a6395f9
Abhay Singh <93770368+Abhay-Singh-1913@users.noreply.github.com>
2024-01-22 18:22:20 +0530

    Update Student_Management_System.java

--- src/main/java/com/internwings/sms/view/Student_Management_System.java
+++ src/main/java/com/internwings/sms/view/Student_Management_System.java
@@ -1,7 +1,16 @@
 package com.internwings.sms.view;
 
 public class Student_Management_System {
-	
+	static Scanner myInput = new Scanner(System.in);
+	static StudentController controller = new StudentController();
+
+	static {
+		System.out.println("//////////////////////////////////////////////////////////");
+		System.out.println("/////                                                /////");
+		System.out.println("/////----- WELCOME TO STUDENT MANAGEMENT SYSTEM -----/////");
+		System.out.println("/////                                                /////");
+		System.out.println("//////////////////////////////////////////////////////////");
+	}
 	public static void main(String[] args) {
 		
 	}
